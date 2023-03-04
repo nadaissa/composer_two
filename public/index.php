429e8151e8a1a3a8b19188e0b8d2d26833e267c7
nadaissa <nada-issa_student2023@wilder.school>
2023-03-04 14:30:54 +0100

    challenge composer 3 done

--- public/index.php
+++ public/index.php
@@ -2,8 +2,23 @@
 
 require '../vendor/autoload.php';
 
-use App\Hello;
+//code de l'exercice composer 2
+// use App\Hello;
 
-$sayHello = new Hello('Hello World!');
+// $sayHello = new Hello('Hello World!');
 
-echo $sayHello->getSentence();
+// echo $sayHello->getSentence();
+
+//code de la documentation gipetto pour l'exercice composer 3
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+$output = $bessie->say();
+echo $output;
+
+// // or just echo the object for direct output
+// echo $bessie;
+
+?>
